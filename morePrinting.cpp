170b5477f27669686d71d309de977d5548a3b67a
srishav9 <rishav9471@gmail.com>
2021-03-22 23:15:10 +0530

    added printing

---
+++ morePrinting.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<"I am Rishav";
+    return 0;
+    /*
+
+    Why other lines not
+    printed?    Return 0 tells compiler we are done.
+    Lines after it are
+    ignored!
+    Be careful!
+
+    */
+	cout<<endl<<"I was born in Ghy"
+	    <<endl<<"I graduated from JEC"
+	    <<endl;
+}
---
+++ printing.cpp
@@ -0,0 +1,82 @@
+#include <iostream> //iostream is the one who knocks(prints :P)
+using namespace std;
+
+int main(){
+    
+    //Type I
+    cout<<"I am Rishav"<<endl<<"I was born in Ghy"<<endl<<"I graduated from JEC"<<endl;
+    
+    //Type II
+    cout<<"I am Rishav"
+		<<endl<<"I was born in Ghy"
+		<<endl<<"I graduated from JEC"
+		<<endl;
+    
+    //Type III
+    cout<<"I am Rishav\nI was born in Ghy\nI graduated from JEC\n";
+
+
+    cout<<1<<"\n";
+  	cout<<12<<"\n";
+	  cout<<123<<" is a number\n";
+	  cout<<"123"<<" is NOT a number\n";
+	  cout<<endl;
+
+	  cout<<15.7<<"\n";
+  	cout<<-12.52<<"\n";
+  	cout<<endl;
+
+	  cout<<1234<<"\n";
+  	cout<<4321<<"\n\n";
+
+  	cout<<43211234<<"\n";
+  	cout<<4321<<1234<<"\n";
+
+  	cout<<"\nMy lucky number is: "<<17<<"\n";
+
+
+  	cout<<1<<"\n";
+   	cout<<1+2<<"\n";
+  	cout<<1+2+3<<"\n";
+  	cout<<1+2+3+4<<"\n";
+  	cout<<"1+2+3+4"<<"\n";
+  	cout<<"1+2+3+4="<<1+2+3+4<<"\n\n";
+
+	  cout<<1+2+3+4-10<<"\n";
+	  cout<<17-10<<"\n\n";
+
+	  cout<<"2*3*4 = "<<2*3*4<<"\n";
+  	cout<<"12/2 = "<<12/2<<"\n";
+	  cout<<"12/2/3 = "<<12/2/3<<"\n\n";
+
+	  cout<<"6/2 = "<<6/2<<"\n";
+	  cout<<"7/2 = "<<7/2<<"\n";
+  	cout<<"7.0/2.0 = "<<7.0/2.0<<"\n\n";
+
+  	cout<<10*10*10*10*10*10*10*10*10<<"\n";
+  	cout<<10*10*10*10*10*10*10*10*10*10<<"\n";  //overflow
+
+  cout<<"If your dream only includes you, it’s too small\n\n";
+
+	cout<<30+20+10-10-20-30<<endl;
+	cout<<"70/10"<<"\n";
+	cout<<80/10/2<<"\n";
+
+	cout<<"\nOpportunities don't happen. "
+		<<"You create them"
+		<<endl;
+
+	// All progress takes place outside the comfort zone.
+
+	/*
+		Character cannot be developed in ease and quiet. Only through
+			experience of trial and suffering can the soul be strengthened,
+				ambition inspired, and success achieved
+	 */
+
+	return 0;
+
+	cout<<"\n\nDo one thing every day that scares you";    
+
+	return 0;
+}
