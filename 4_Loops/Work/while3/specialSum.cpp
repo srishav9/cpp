3e6f3603ba1f0889018dc8ebdf8f25978b22340d
srishav9 <rishav9471@gmail.com>
2022-06-12 13:47:00 +0530

    added special sum

--- 4_Loops/Work/while3/specialSum.cpp
+++ 4_Loops/Work/while3/specialSum.cpp
@@ -5,19 +5,18 @@ int main(){
     cout<<"Enter tests: ";
     cin>>T;
     while(T>0){
-        int itr, cnt=1, sum=0;
+        int N, cnt=1, sum=0;
         cout<<"Enter N ints: ";
-        cin>>itr;
-        while(itr>0){
+        cin>>N;
+        while(N>0){
             int num, prod=1, i=0;
             cin>>num;
             while(i<cnt){
-                prod = prod*num;
-                i++;
+                prod *= num, i++;
             }
             sum += prod;
             cnt++;
-            itr--;
+            N--;
         }
         cout<<"Special sum is: "<<sum<<" "<<endl;
         T--;
